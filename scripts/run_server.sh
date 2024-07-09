5c9fb8c75f6f3fcb9ddc33af24215bc463bfdf20
Brandon Sprague <brandon@siliconally.org>
2024-07-09 16:54:56 -0700

    Add encrypted allowlists back to the repo

--- scripts/run_server.sh
+++ scripts/run_server.sh
@@ -23,10 +23,26 @@ OPTS=$(getopt \
   -- "$@"
 )
 
+if ! [ -x "$(command -v sops)" ]; then
+  echo 'Error: sops is not installed.' >&2
+  exit 1
+fi
+
+TMP_CONFIG_DIR="$(mktemp -d -t credsrv-local-XXXXXXXXX)"
+function cleanup {
+  rm -rf "$TMP_CONFIG_DIR"
+}
+trap cleanup EXIT
+TMP_ALLOWLIST_FILE="${TMP_CONFIG_DIR}/local.json"
+TMP_CONFIG_FILE="${TMP_CONFIG_DIR}/local.conf"
+sops -d "$ROOT/cmd/server/configs/allowlists/local.enc.json" > "$TMP_ALLOWLIST_FILE"
+cp "$ROOT/cmd/server/configs/local.conf" "$TMP_CONFIG_FILE"
+printf "\nallowlist_file %s\n" "$TMP_ALLOWLIST_FILE" >> "$TMP_CONFIG_FILE"
+
 eval set --$OPTS
 
 declare -a FLAGS=(
-  "--config=cmd/server/configs/local.conf"
+  "--config=$TMP_CONFIG_FILE"
 )
 while [ ! $# -eq 0 ]
 do
